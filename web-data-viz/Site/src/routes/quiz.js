a51593dc2a795ab9e73dbe943bd6706dba7b6d52
joaovictoradsb <joao.lsilva@sptech.school>
2025-05-30 17:26:31 -0300

    update: API Quiz

--- web-data-viz/Site/src/routes/quiz.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var quizController = require("../controllers/quizController");
-
-
-router.get("/perguntas/:fkQuiz", quizController.buscarPerguntasPorQuiz);
-router.post("/cadastrarPontos", function (req, res) {quizController.cadastrarPontos(req, res);});
-
-module.exports = router;
